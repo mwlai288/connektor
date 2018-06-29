6e84804b7d801efa9deba93f0b8e166bf676473a
mwlai288 <mwlai288@gmail.com>
2018-06-28 22:21:01 -0400

    created education and experience component. added more reducers and actions. finished dashboard and profile component

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,47 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser, logOutUser } from "./actions/authActions";
+import { clearCurrentProfile } from "./actions/profileActions";
+
 import { Provider } from "react-redux";
 import store from "./store";
+
 import Navbar from "./components/layout/Navbar";
-import Landing from "./components/layout/Landing";
 import Footer from "./components/layout/Footer";
+import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 
 import "./App.css";
+import Dashboard from "./components/dashboard/Dashboard";
+import PrivateRoute from "./components/common/PrivateRoute";
+import CreateProfile from "./components/create-profile/CreateProfile";
+import EditProfile from "./components/editprofile/EditProfile";
+import AddExperience from "./components/add-credentials/AddExperience";
+import AddEducation from "./components/add-credentials/AddEducation";
+
+// Check for token
+if (localStorage.jwtToken) {
+	// Set auth token header auth
+	setAuthToken(localStorage.jwtToken);
+	// Decode token and get user info and exp
+	const decoded = jwt_decode(localStorage.jwtToken);
+	// Set user and isAuthenticated
+	store.dispatch(setCurrentUser(decoded));
+
+	// Check for expired token
+	const currentTime = Date.now() / 1000;
+	if (decoded.exp < currentTime) {
+		// Logout user
+		store.dispatch(logOutUser());
+		// Clear current Profile
+		store.dispatch(clearCurrentProfile());
+		// Redirect to login
+		window.location.href = "/login";
+	}
+}
 
 class App extends Component {
 	render() {
@@ -21,6 +54,29 @@ class App extends Component {
 						<div className="container">
 							<Route exact path="/register" component={Register} />
 							<Route exact path="/login" component={Login} />
+							<Switch>
+								<PrivateRoute exact path="/dashboard" component={Dashboard} />
+								<PrivateRoute
+									exact
+									path="/create-profile"
+									component={CreateProfile}
+								/>
+								<PrivateRoute
+									exact
+									path="/edit-profile"
+									component={EditProfile}
+								/>
+								<PrivateRoute
+									exact
+									path="/add-experience"
+									component={AddExperience}
+								/>
+								<PrivateRoute
+									exact
+									path="/add-education"
+									component={AddEducation}
+								/>
+							</Switch>
 						</div>
 						<Footer />
 					</div>
